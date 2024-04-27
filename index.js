c6af988b3f13e71a00d7e34faa1d39ad3e0237dd
Meathill <meathill@gmail.com>
2024-04-27 16:56:39 +0800

    change: refactor to fit Bright Data

--- index.js
+++ index.js
@@ -7,64 +7,75 @@ import 'dotenv/config.js';
 console.log('Starting scraper');
 const AUTH = process.env.BRIGHT_DATA_AUTH;
 const SBR_WS_ENDPOINT = `wss://${AUTH}@${process.env.BRIGHT_DATA_SBR_WS_ENDPOINT}`;
-const browser = process.env.DEV
-  ? await puppeteer.launch({
-    headless: false,
-    userDataDir: './user_data',
-  })
-  : await puppeteerCore.connect({ browserWSEndpoint: SBR_WS_ENDPOINT });
 
-console.log('Browser connected');
-const page = await browser.newPage();
-// Set screen size
-if (process.env.DEV) {
-  await page.setViewport({width: 1280, height: 1373});
-}
+async function fetchList() {
+  const browser = process.env.DEV
+    ? await puppeteer.launch({
+      headless: false,
+      userDataDir: './user_data',
+    })
+    : await puppeteerCore.connect({ browserWSEndpoint: SBR_WS_ENDPOINT });
 
-// Navigate the page to a URL
-await page.goto('https://vuejobs.com/jobs', {
-  timeout: 120000,
-  waitUntil: 'domcontentloaded',
-});
-console.log('Page loaded');
-
-// click "remote" switch
-const remoteSwitch = await page.$('button[role="switch"]');
-await remoteSwitch.click();
-
-// only filter full-time
-await page.click('.n-base-selection-tags');
-await page.click('.n-base-select-option');
-
-// wait for list to be loaded
-await sleep(5000);
-console.log('List loaded');
-
-// check all links and find the ones to detail pages
-const allLinks = await page.$$('a');
-const globalRemoteJobLinks = [];
-for (const link of allLinks) {
-  // must be job detail page
-  const href = await link.evaluate((node) => node.getAttribute('href'));
-  if (!href || !href.startsWith('/jobs/')) continue;
-
-  const badgeIcons = await link.$$('img.h-3');
-  const hasCountry = badgeIcons.some(async (badgeIcon) => {
-    const alt = await badgeIcon.evaluate((node) => node.getAttribute('alt'));
-    return alt.startsWith('Flag of ');
-  });
-
-  if (!hasCountry) {
-    globalRemoteJobLinks.push(href);
+  console.log('Browser connected');
+  const page = await browser.newPage();
+  // Set screen size
+  if (process.env.DEV) {
+    await page.setViewport({width: 1280, height: 1373});
   }
+
+  // Navigate the page to a URL
+  await page.goto('https://vuejobs.com/jobs', {
+    timeout: 120000,
+    waitUntil: 'domcontentloaded',
+  });
+  console.log('Page loaded');
+
+  // click "remote" switch
+  const remoteSwitch = await page.$('button[role="switch"]');
+  await remoteSwitch.click();
+
+  // only filter full-time
+  await page.click('.n-base-selection-tags');
+  await page.click('.n-base-select-option');
+
+  // wait for list to be loaded
+  await sleep(5000);
+  console.log('List loaded');
+
+  // check all links and find the ones to detail pages
+  const allLinks = await page.$$('a');
+  const globalRemoteJobLinks = [];
+  for (const link of allLinks) {
+    // must be job detail page
+    const href = await link.evaluate((node) => node.getAttribute('href'));
+    if (!href || !href.startsWith('/jobs/')) continue;
+
+    const badgeIcons = await link.$$('img.h-3');
+    const hasCountry = badgeIcons.some(async (badgeIcon) => {
+      const alt = await badgeIcon.evaluate((node) => node.getAttribute('alt'));
+      return alt.startsWith('Flag of ');
+    });
+
+    if (!hasCountry) {
+      globalRemoteJobLinks.push(href);
+    }
+  }
+
+  await page.close();
+  await browser.close();
+  console.log('Found', globalRemoteJobLinks.length, 'remote job links');
+  return globalRemoteJobLinks;
 }
 
-await page.close();
-console.log('Found', globalRemoteJobLinks.length, 'remote job links');
+async function fetchDetail(href) {
+  const browser = process.env.DEV
+    ? await puppeteer.launch({
+      headless: false,
+      userDataDir: './user_data',
+    })
+    : await puppeteerCore.connect({ browserWSEndpoint: SBR_WS_ENDPOINT });
+  console.log('Browser connected');
 
-// fetch all job details
-const jobDetails = [];
-for (const href of globalRemoteJobLinks) {
   const url = `https://vuejobs.com${href}`;
   console.log('Fetching job details from', url);
   const newPage = await browser.newPage();
@@ -109,12 +120,20 @@ for (const href of globalRemoteJobLinks) {
     return container.innerHTML;
   });
 
-  jobDetails.push({timezone, content});
   await newPage.close();
+  await browser.close();
   console.log('Job details fetched:', url);
+
+  return {timezone, content};
+}
+
+const globalRemoteJobLinks = await fetchList();
+// fetch all job details
+const jobDetails = [];
+for (const href of globalRemoteJobLinks) {
+  const detail = await fetchDetail(href);
+  jobDetails.push(detail);
 }
 
 await writeFile('jobs.json', JSON.stringify(jobDetails, null, 2), 'utf8');
-
-
-await browser.close();
+console.log('done');
